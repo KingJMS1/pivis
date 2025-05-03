bda72550e91b622c0ca99ae4b13ffb0adeca6c69
KingJMS1 <1kingjms@gmail.com>
2025-05-03 23:46:28 +0000

    Fix docs

--- docs/build.sh
+++
@@ -1,3 +0,0 @@
-cp index.rst ./source/index.rst
-sphinx-apidoc -f -o ./source ../src
-sphinx-build -b html -c . ./source ./built
--- docs/clean.sh
+++
@@ -1,4 +0,0 @@
-rm -rf source
-rm -rf built
-mkdir source
-mkdir built
