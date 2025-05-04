4089337d7ec49894db701b31f2732c45cbde1241
KingJMS1 <1kingjms@gmail.com>
2025-05-04 00:11:08 +0000

    Attempt to fix docs again

--- _buildocs/build.sh
+++ _buildocs/build.sh
@@ -1,4 +1,5 @@
 cp index.rst ./source/index.rst
 sphinx-apidoc -f -o ./source ../src
 sphinx-build -b html -c . ./source ./built
+touch ./built/.nojekyll
 mv ./built ../docs
--- _buildocs/clean.sh
+++ _buildocs/clean.sh
@@ -1,4 +1,5 @@
 rm -rf source
 rm -rf built
+rm -rf ../docs
 mkdir source
 mkdir built
